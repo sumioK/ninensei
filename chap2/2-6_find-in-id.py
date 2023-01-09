c242210690fa01752b29b314a75eea9059ce4bc4
Sumio K <tap.miwo@gmail.com>
2023-01-10 06:20:06 +0900

    find all in id

---
+++ chap2/2-6_find-in-id.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+load_url = 'https://www.ymori.com/books/python2nen/test2.html'
+html = requests.get(load_url)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+chap2 = soup.find(id='chap2')
+for element in chap2.find_all('li'):
+  print(element.text)
