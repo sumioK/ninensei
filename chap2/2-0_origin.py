96efbbab81fce181968bb5443009b373796894e4
Sumio K <tap.miwo@gmail.com>
2023-01-11 22:00:13 +0900

    url

--- chap2/2-0_origin.py
+++ chap2/2-0_origin.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib
 
 load_url = 'https://piso-animals.com/posts/index'
 html = requests.get(load_url)
@@ -7,3 +8,6 @@ soup = BeautifulSoup(html.content, 'html.parser')
 
 for element in soup.find_all('a'):
   print(element.text)
+  url = element.get('href')
+  link_url = urllib.parse.urljoin(load_url, url)
+  print(link_url)
