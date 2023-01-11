2e551561eee14f57531f0ae0ee76170c2f181265
Sumio K <tap.miwo@gmail.com>
2023-01-12 06:52:15 +0900

    dl all content

---
+++ chap2/2-12_dl-all.py
@@ -0,0 +1,14 @@
+import requests
+from pathlib import Path
+
+out_folder = Path('download')
+out_folder.mkdir(exist_ok=True)
+
+image_url = 'https://www.ymori.com/books/python2nen/sample1.png'
+image_data = requests.get(image_url)
+
+filename = image_url.split('/')[-1]
+out_path = out_folder.joinpath(filename)
+
+with open(out_path, mode='wb') as f :
+  f.write(image_data.content)
