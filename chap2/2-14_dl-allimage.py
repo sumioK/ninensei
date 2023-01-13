ea6bbf10720792071dfc21380bc3e27dc3150aa8
Sumio K <tap.miwo@gmail.com>
2023-01-13 21:40:41 +0900

    dl all img

--- chap2/2-14_dl-allimage.py
+++ chap2/2-14_dl-allimage.py
@@ -8,20 +8,25 @@ load_url = 'https://www.ymori.com/books/python2nen/test2.html'
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, 'html.parser')
 
+# 保存用ディレクトリの作成
 out_folder = Path("downloads2")
 out_folder.mkdir(exist_ok=True)
 
+# すべてのimgタグを検索しリンクを取得
 for element in soup.find_all('img'):
   src = element.get('src')
 
+  # 絶対URLを作って画像データを取得する
   image_url = urllib.parse.urljoin(load_url, src)
   imgdata = requests.get(image_url)
 
+  # URLから最後のファイル名を取り出して、保存フォルダとつなげる
   filename = image_url.split('/')[-1]
   out_path = out_folder.joinpath(filename)
   
+  # 画像データをファイルに出力
   with open(out_path, mode='wb') as f:
     f.write(imgdata.content)
 
+    # １秒停止
     time.sleep(1)
-    
