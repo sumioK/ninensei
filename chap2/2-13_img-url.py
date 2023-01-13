858735ab9c76ac50f49b04e90a8232cccc8aefca
Sumio K <tap.miwo@gmail.com>
2023-01-13 21:46:54 +0900

    ans

--- chap2/2-13_img-url.py
+++ chap2/2-13_img-url.py
@@ -7,8 +7,10 @@ html = requests.get(load_url)
 soup = BeautifulSoup(html.content, 'html.parser')
 
 for element in soup.find_all('img'):
+  # ここで定義するsrcはimgタグのsrc=以降
   src = element.get('src')
 
+# urljoinは基底URLと別のURLを結合して絶対URLを作る
   image_url = urllib.parse.urljoin(load_url, src)
   filename = image_url.split('/')[-1]
   print(image_url, '>>', filename)
